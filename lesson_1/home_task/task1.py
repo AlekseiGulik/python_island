07e546d7fb4c2404ecbada5fa775a408c389d368
admin <alexygulik@gmail.com>
2024-10-01 18:51:57 +0300

    first_try

--- lesson_1/home_task/task1.py
+++ lesson_1/home_task/task1.py
@@ -1,7 +1,13 @@
 # todo: Определить в коде переменные:
 # 1. Целочисленного типа
+number = 777
 # 2. Вещественного типа
+digit = 77.7
 # 3. Логического типа
+boolean = True
 # 4. Строкового типа
+word = "word"
 # 5. Пустого типа
+empty = None
 # Вывести их типы. 123
+print( type(number), type(digit), type(boolean), type(word), type(None) )
--- lesson_1/home_task/task2.py
+++ lesson_1/home_task/task2.py
@@ -1,17 +1,43 @@
 # todo: Преобразуйте переменную age и foo в число
 # age = "23"
 # foo = "23abc"
-#
+from lesson_1.home_task.task1 import boolean
+
+# age = "23"
+# age = int(age)
+# print(type(age))
+
+# foo = "23abc"
+# foo = int(foo)
+# print(type(foo)) #Возникает ошибка: что строка содержит нечисловые символы, поэтому её невозможно преобразовать в int
+
 # Преобразуйте переменную age в Boolean
 # age = "123abc"
-#
+
+# age = "123abc"
+# age = bool(age)
+# print(type(age))
+
 # Преобразуйте переменную flag в Boolean
 # flag = 1
-#
+# flag = 1
+# flag = bool(flag)
+# print(type(flag), bool(flag))
+
 # Преобразуйте значение в Boolean
 # str_one = "Privet"
 # str_two = ""
-#
+# str_one = "Privet"
+# str_one = bool(str_one)
+# print(type(str_one), bool(str_one))
+
+str_two = ""
+str_two = bool(str_two)
+print(type(str_two), bool(str_two))
+
 # Преобразуйте значение 0 и 1 в Boolean
+x = 0
+y = 1
+
 #
 # Преобразуйте False в строку

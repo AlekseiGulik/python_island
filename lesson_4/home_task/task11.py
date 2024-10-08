017ffad34e072ff84aa2e58f042f75768304e938
Aleksei <alekseygulik@gmail.com>
2024-10-08 22:22:53 +0300

    first1

--- lesson_4/home_task/task11.py
+++ lesson_4/home_task/task11.py
@@ -1,2 +1,12 @@
 # todo: Дан номер некоторого года (положительное целое число).
 # Вывести соответствующий ему номер столетия, учитывая, что, к примеру, началом 20 столетия был 1901 год.
+
+year = int(input("Введите год: "))
+def get_century(year):
+    if year % 100 == 0:
+        century = year // 100
+    else:
+        century = (year // 100) + 1
+    return century
+century = get_century(year)
+print(f"{year} год соответствует {century} столетию.")
